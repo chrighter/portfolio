b1e7aeb090d38d89a8148300d0ff512bc6d31466
chrighter <christina.ilyenko@yandex.ru>
2016-11-02 19:20:18 +0500

    initial commit

---
+++ app/views.py
@@ -0,0 +1,38 @@
+# from app import app
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Hello, World!"
+
+# простое представление, которое просто возвращает строку для отображения в пользовательском браузере. 
+# Два декоратора route создают привязку адресов / и /index к этой функции.
+
+# from flask import render_template
+# from app import app
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     user = { 'nickname': 'Miguel' } # выдуманный пользователь
+#     return render_template("index.html",
+#         title = 'Home',
+#         user = user)
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/main')
+def main():
+    return render_template('main.html', title = 'main')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'about')
+
+@app.route('/pictures')
+def pictures():
+    return render_template('pictures.html', title = 'pictures')
+
+
